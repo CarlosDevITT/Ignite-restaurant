3bbe16d1f14c43befc5bea44bf3fb33451ba642a
Carlos Domingos <99830819+CarlosDevITT@users.noreply.github.com>
2026-09-13 12:20:18 -0300

    feat(profile): wire customer purchase preferences

--- Cardapio/js/modules/mobile-ux.js
+++ Cardapio/js/modules/mobile-ux.js
@@ -1,6 +1,7 @@
 import { initCartUX } from './cart-ux.js';
 import { initPurchaseFeedback } from './purchase-feedback.js';
 import { initCheckoutAddressBook } from './checkout-address-book.js';
+import { initCustomerPreferences } from './customer-preferences.js';
 
 const KEYBOARD_THRESHOLD = 140;
 
@@ -97,5 +98,6 @@ export function initMobileUX() {
   setupCategoryFilterSheet();
   initCartUX();
   initCheckoutAddressBook();
+  initCustomerPreferences();
   initPurchaseFeedback();
 }
